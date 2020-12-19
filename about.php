4157e517fee1d55320aa490652c13b6070c049a8
Lance <60122560+LanceVaflor@users.noreply.github.com>
2020-12-19 22:51:31 +0800

    Add files via upload

---
+++ about.php
@@ -0,0 +1,17 @@
+<html>
+        <head>
+                <title> About the Author</title>
+                <link rel="stylesheet" type="text/css" href="style.css">
+        </head>
+        
+        <body>
+        
+          
+          <div class="about_content">
+            <h1><center>About the Author</center><h1>
+                <h3>Hello! I am Lance Daimler Vaflor. I am a BSIT 3rd year student. This is my portfolio for animation.
+                    <br>You are welcome to check out my work! I am actually a novice in this field of animation. This is my
+                    first time working on SVG and Canvas. <br> I have no actual work experience as of now, but hopefully soon enough. <br> </h3>
+          </div>
+        </body>  
+</html>             
---
+++ home.php
@@ -0,0 +1,24 @@
+<html>
+        <head>
+                <title> Home</title>
+                <link rel="stylesheet" type="text/css" href="style.css">
+        </head>
+        
+        <body>
+          
+          <div class="home_content">
+                <div class ="deathstar">
+                 <object type="image/svg+xml" data="svg/deathstar.svg"></object>
+                 </div>
+                 <div class ="watchmen">
+                 <object type="image/svg+xml" data="svg/watchmen.svg"></object>
+                 </div>
+                 
+            <h1><center>HOME</center></h1>
+
+           
+          </div>
+        
+          
+        </body>  
+</html>             
